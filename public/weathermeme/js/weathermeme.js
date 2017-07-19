c927867791e9c0681b6b4d23f280d3de985ff310
andrewbeav <andrewbevelhymer@gmail.com>
2017-07-18 21:31:05 -0500

    Improve (slightly) UI of weathermeme implementation

--- public/weathermeme/js/weathermeme.js
+++ public/weathermeme/js/weathermeme.js
@@ -1,6 +1,13 @@
 const owm_appid = '5f0dabe0a94eb1d907d7a79ff7562b93';
 
 $(function() {
+  navigator.geolocation.getCurrentPosition(updateMeme, function(err) {
+    console.log("Error getting location");
+  },
+  {
+    enableHighAccuracy: true
+  });
+
   $('#memeGenBtn').on('click', function() {
     $('#meme').attr('src', 'loading.gif');
     navigator.geolocation.getCurrentPosition(updateMeme, function(err) {
@@ -18,5 +25,9 @@ function updateMeme(position) {
     console.log(data.meme_location);
     $('#meme').attr('src', data.meme_location);
     $('#locationLabel').html(data.weather_info.name);
+    $('#tmpLabel').html('Temp: ' + data.weather_info.main.temp + ' &#176;F');
+    $('#windLabel').html('Wind: ' + data.weather_info.wind.speed + 'MPH')
+    $('#humidityLabel').html('Humidity: ' + data.weather_info.main.humidity + '%');
+    $('#weatherDescriptionLabel').html(data.weather_info.weather[0].description);
   });
 }
