c8ebbf1a4a754822aa814ada4cd770dc4ad61ac7
andrewbeav <andrewbevelhymer@gmail.com>
2017-07-27 19:51:30 -0500

    Make https work

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var path = require('path');
 var cors = require('cors');
+var fs = require('fs');
+var https = require('https');
 
 var app = express();
 
@@ -9,7 +11,16 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
+// Helmet middleware for Securing headers
+app.use(require('helmet')());
+
 app.use('/weathermeme', require('./routes/weathermeme'));
 
+const options = {
+  cert: fs.readFileSync('./sslcert/fullchain.pem'),
+  key: fs.readFileSync('./sslcert/privKey.pem')
+};
+
 app.listen(3000);
+https.createServer(options, app).listen(8443);
 console.log('Listening on port 3000');
